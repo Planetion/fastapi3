1bebf51619c2f151a35f55ae16ae976e0c6eb157
Planetion <160325967+Planetion@users.noreply.github.com>
2024-04-30 20:42:52 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 DB_HOST=os.environ.get("DB_HOST")
-DB_PORT=5432
+DB_PORT=os.environ.get("DB_PORT")
 DB_NAME=os.environ.get("DB_NAME")
 DB_USER=os.environ.get("DB_USER")
 DB_PASS=os.environ.get("DB_PASS")  
@@ -12,5 +12,5 @@ DB_PASS=os.environ.get("DB_PASS")
 DB_NAME_TEST=os.environ.get("DB_NAME_TEST")
 DB_USER_TEST=os.environ.get("DB_USER_TEST")
 DB_PASS_TEST=os.environ.get("DB_PASS_TEST")
-DB_PORT_TEST=5432
+DB_PORT_TEST=os.environ.get("DB_PORT_TEST")
 DB_HOST_TEST=os.environ.get("DB_HOST_TEST")
